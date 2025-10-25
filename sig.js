e63284dc2488ebb16e91e286afc9a8470653749f
samseasons <185178474+samseasons@users.noreply.github.com>
2025-10-25 10:00:07 +0000

    Add files via upload

--- sig.js
+++ sig.js
@@ -1,4 +1,12 @@
-a0 = 0, a1 = 1, a2 = 2, a5 = 5, a16 = 16, a32 = 32, a64 = 64, a160 = 160, a192 = 192, a16384 = 16384, b0 = 0n, b4 = 4n
+a0 = 0, a1 = 1, a16 = 16, a32 = 32, a64 = 64, a160 = 160, a162 = 162, a192 = 192, a15552 = 15552, b0 = 0n, b4 = 4n
+
+function big (a) {
+  return BigInt(a)
+}
+
+function num (a) {
+  return Number(a)
+}
 
 function uint8 (a) {
   return new Uint8Array(a)
@@ -30,15 +38,15 @@ function shift (a, b) {
 }
 
 function expand (a, g=a0, h=a1) {
-  g = BigInt(g)
-  h = BigInt(h)
+  g = big(g)
+  h = big(h)
   if (g >= h) {
     return uint8(a0)
   }
   a = pack(a)
-  let i = uint32(a16).map((c, b) => a[b] | a0 + a[b + a16] | a0)
-      j = uint32(a16).map((c, b) => a[b + a32] | a0)
-  a = uint8(Number(h - g))
+  let i = uint32(a16).map((c, b) => a[b] | a0),
+      j = uint32(a16).map((c, b) => a[b + a16] | a0)
+  a = uint8(num(h - g))
 
   function k (a, b, c, d, e, g=a[b], h=a[c], i=a[d], j=a[e]) {
     g = shift(g ^ h, i)
@@ -49,14 +57,14 @@ function expand (a, g=a0, h=a1) {
     i += j
     j = shift(j ^ g, h)
     g += h
-    a[b] = g + 1
-    a[c] = h + 1
-    a[d] = i + 1
-    a[e] = j + 1
+    a[b] = g + a1
+    a[c] = h + a1
+    a[d] = i + a1
+    a[e] = j + a1
   }
 
   function l () {
-    let a = i.slice(), b = i.slice(), c, d = 16, e = 25
+    let a = i.slice(), b = j.slice(), c, d = a16, e = 10
 
     function m (a) {
       k(a, 0, 4, 8, 12)
@@ -71,7 +79,8 @@ function expand (a, g=a0, h=a1) {
 
     while (e--) {
       m(a)
-      if (e % a5 == a0) {
+      m(b)
+      if (e % 5 == a0) {
         c = d
         while (c--) {
           b[c] = a[c] += b[c]
@@ -85,17 +94,17 @@ function expand (a, g=a0, h=a1) {
   let m = 2n ** 32n
 
   function n (a) {
-    let b = b0, c = a0, d = a16
-    while (c < d) {
-      b = b * m + BigInt(a[c++])
+    let b = a0, c = a16, d = 0n
+    while (b < c) {
+      d = d * m + big(a[b++])
     }
-    return b
+    return d
   }
 
   function o (a, b) {
-    let c = 15, d = a0
-    while (c > d) {
-      b[c--] = Number(a % m)
+    let c = a0, d = a16
+    while (c < d) {
+      b[--d] = num(a % m)
       a /= m
     }
     return b
@@ -105,7 +114,7 @@ function expand (a, g=a0, h=a1) {
   i = o(n(i) + q, uint32(a16))
   j = o(n(j) + q, uint32(a16))
   m = g % p
-  a.set(unpack(l()).slice(Number(m), Number(m + h - g)))
+  a.set(unpack(l()).slice(num(m), num(h - g + m)))
   for (let b = g - m + p, c; b < h; b += p) {
     i[c = 15]++
     while (i[c] == a0) {
@@ -115,67 +124,46 @@ function expand (a, g=a0, h=a1) {
     while (j[c] == a0) {
       j[--c]++
     }
-    a.set(unpack(l()).slice(a0, Number(h - b)), Number(b - g))
+    a.set(unpack(l()).slice(a0, num(h - b)), num(b - g))
   }
   return a
 }
 
 function reduce (a, h=a1) {
-  let b, c = a.length, d
-  while (c > a32) {
-    b = []
-    while (c > a0) {
-      d = c / a2
-      b.push(...expand([...a.slice(a0, a32), ...a.slice(d, d + a32)], a0, a32))
-      a = [...a.slice(a32, d), ...a.slice(d + a32)]
-      c = a.length
-    }
-    a = b.slice()
-    c = a.length
+  while (a.length > a64) {
+    a = [...expand(a.slice(a0, a64), a0, a32), ...a.slice(a64)]
   }
   return expand(a, a0, h)
 }
 
 function bigint (a) {
-  let b = b0, c = 256n, l = a.length - a1
-  for (let i = l; i >= a0; i--) {
-    b += BigInt(a[i]) * (c ** BigInt(l - i))
+  let b = b0
+  for (let c = 256n, i = a0, l = a.length - a1; i <= l; i++) {
+    b += big(a[i]) * c ** big(l - i)
   }
   return b
 }
 
-function root (a) {
-  return BigInt((a - 1n) / b4)
-}
-
 function branches (a) {
-  const b = []
-  let c = a * b4 + 1n
+  const b = [], c = a * b4 + 1n
   for (let i = b0; i < b4; i++) {
     b.push(c + i)
   }
   return b
 }
 
-function strbits (a) {
-  return ('00000000' + a.toString(a2)).substr(-8)
+function root (a) {
+  return big((a - 1n) / b4)
 }
 
-function bytebits (a) {
-  a = strbits(a).split('')
-  const b = []
-  for (let i = a.length - a1; i >= a0; i--) {
-    b.push(parseInt(a[i]))
+function trits (a) {
+  a = bigint(a)
+  let b = 3n, c = a0, d = a162, e = uint8(d)
+  while (c < d) {
+    e[--d] = num(a % b)
+    a /= b
   }
-  return b
-}
-
-function bits (a) {
-  const b = []
-  for (let i = a0, l = a.length; i < l; i++) {
-    b.push(...bytebits((a[i])))
-  }
-  return b
+  return e
 }
 
 function randombytes (a) {
@@ -183,63 +171,78 @@ function randombytes (a) {
 }
 
 function gen (a=randombytes(a32)) {
-  a = expand(a, a0, a16384 + a64)
-  const c = uint8(a16384)
-  for (let i = a0, j, k, l = 256; i < l; i++) {
-    j = i * a64
+  a = expand(a, a0, a15552)
+  const c = uint8(a15552)
+  for (let i = a0, j = a0, k, l; i < a162; i++) {
     k = j + a32
+    l = k + a32
     c.set(expand(a.slice(j, k), a0, a32), j)
-    c.set(expand(a.slice(k, k + a32), a0, a32), k)
+    c.set(expand(a.slice(k, l), a0, a32), k)
+    j = l + a32
+    c.set(expand(a.slice(l, j), a0, a32), l)
   }
   return reduce(c, a32)
 }
 
 function sign (a, b) {
-  a = bits(a)
-  const c = uint8(a16384), d = uint8(a16384)
-  const e = expand(b, a0, a16384 + a64)
-  b = uint8(a16384 + a32)
-  for (let i = a0, j, k, l = 256; i < l; i++) {
-    j = i * a64
+  a = trits(a)
+  const c = uint8(a32 + a15552), d = uint8(a15552), e = expand(b, a0, a15552)
+  for (let i = a0, j, k, l, m = a0; i < a162; i++) {
+    j = m
     k = j + a32
+    l = k + a32
+    m = l + a32
     d.set(expand(e.slice(j, k), a0, a32), j)
-    d.set(expand(e.slice(k, k + a32), a0, a32), k)
-    if (a[i]) {
-      c.set(d.slice(j, k), j)
-      c.set(e.slice(k, k + a32), k)
+    d.set(expand(e.slice(k, l), a0, a32), k)
+    d.set(expand(e.slice(l, m), a0, a32), l)
+    if (a[i] == a0) {
+      c.set(e.slice(j, k), k)
+      c.set(d.slice(k, l), l)
+      c.set(d.slice(l, m), m)
+    } else if (a[i] == a1) {
+      c.set(d.slice(j, k), k)
+      c.set(e.slice(k, l), l)
+      c.set(d.slice(l, m), m)
     } else {
-      c.set(e.slice(j, k), j)
-      c.set(d.slice(k, k + a32), k)
+      c.set(d.slice(j, k), k)
+      c.set(d.slice(k, l), l)
+      c.set(e.slice(l, m), m)
     }
   }
-  b.set(reduce(d, a32))
-  b.set(c, a32)
-  return b
+  c.set(reduce(d, a32))
+  return c
 }
 
 function valid (a, b, c) {
   if (b.length != a32) {
     return false
   }
-  a = bits(a)
-  const d = uint8(a16384)
-  for (let i = a0, j, k, l = a.length; i < l; i++) {
-    j = i * a64
+  a = trits(a)
+  const d = uint8(a15552)
+  for (let i = a0, j, k, l, m = a0; i < a162; i++) {
+    j = m
     k = j + a32
-    if (a[i]) {
-      d.set(c.slice(j, k), j)
-      d.set(expand(c.slice(k, k + a32), a0, a32), j + a32)
-    } else {
+    l = k + a32
+    if (a[i] == a0) {
       d.set(expand(c.slice(j, k), a0, a32), j)
-      d.set(c.slice(k, k + a32), j + a32)
+      d.set(c.slice(k, l), k)
+      d.set(c.slice(l, m = l + a32), l)
+    } else if (a[i] == a1) {
+      d.set(c.slice(j, k), j)
+      d.set(expand(c.slice(k, l), a0, a32), k)
+      d.set(c.slice(l, m = l + a32), l)
+    } else {
+      d.set(c.slice(j, k), j)
+      d.set(c.slice(k, l), k)
+      d.set(expand(c.slice(l, m = l + a32), a0, a32), l)
     }
   }
-  return reduce(d, a32).toString() == b.toString()
+  return b.toString() == reduce(d, a32).toString()
 }
 
 function sig (a, b, c=a0) {
   if (typeof c == 'number') {
-    c = BigInt(c)
+    c = big(c)
   } else if (typeof c == 'object') {
     if (c.length > a32) {
       c = reduce(c, a32)
@@ -251,38 +254,37 @@ function sig (a, b, c=a0) {
     f.unshift(c = root(c))
   }
   let e, g, h, i, j, k
-  let p = 64n, q = p / 2n
-  const r = {}, s = {}
-  h = branches(f[f.length - a1])
-  g = p * h[h.length - a1]
-  a = reduce(a, a32)
-  c = [...a]
-  c.push(...sign(a, expand(b, g + q, g + p)))
-  const d = []
+  const o = 32n, p = 64n
+  g = branches(f[f.length - a1])
+  h = g[g.length - a1] * p
+  b = reduce(b, a32)
+  c = [...b]
+  c.push(...sign(b, expand(a, h + o, h + p)))
+  const d = [], r = {}, s = {}
   for (i in f.reverse()) {
-    h = f[i]
-    g = p * h
-    if (!(h in r)) {
-      r[h] = expand(b, g, g + q)
+    g = f[i]
+    h = g * p
+    if (!(g in r)) {
+      r[g] = expand(a, h, h + o)
     }
-    e = branches(h)
-    e.filter(i => {
+    e = branches(g)
+    for (i of e) {
       if (!(i in s)) {
-        g = p * i
-        r[i] = expand(b, g, g + q) 
+        h = i * p
+        r[i] = expand(a, h, h + o)
         s[i] = gen(r[i])
       }
-    })
-    g = [...gen(expand(b, g + q, g + p))]
-    j = a0
-    k = Number(b4)
-    while (j < k) {
-      g.push(...s[e[j++]])
     }
-    j = sign(reduce(g, a32), r[h])
-    s[h] = j.slice(a0, a32)
+    h = [...gen(expand(a, h + o, h + p))]
+    j = a0
+    k = num(b4)
+    while (j < k) {
+      h.push(...s[e[j++]])
+    }
+    j = sign(reduce(h, a32), r[g])
+    s[g] = j.slice(a0, a32)
     d.unshift(...j)
-    d.unshift(...g)
+    d.unshift(...h)
   }
   c.push(...d)
   return c
@@ -292,73 +294,72 @@ function verify (a, b, c) {
   try {
     a = reduce(a, a32)
     if (c.slice(a0, a32).toString() != a.toString()) {
-      console.log('bad match', c.slice(a0, a32), a)
+      console.log('bad match', a, c.slice(a0, a32))
       return false
     }
-    c = c.slice(a32)
-    const g = c.slice(a0, a32)
-    c = c.slice(a32)
-    let d, e, f, j, k, s = c.slice(a0, a16384)
-    c = c.slice(a16384)
-    if (!valid(a, g, s)) {
-      console.log('bad first', g, a, s)
+    const d = c.slice(a32, a64)
+    c = c.slice(a64)
+    let e, f, g, j, s = c.slice(a0, a15552)
+    c = c.slice(a15552)
+    if (!valid(a, d, s)) {
+      console.log('bad first', a, d, s)
       return false
     }
     if (c.slice(a160, a192).toString() != b.toString()) {
       console.log('bad public', b, c.slice(a160, a192))
       return false
     }
-    const l = c.length / (a16384 + a192)
+    const l = c.length / (a192 + a15552)
     if (l % a1 != a0) {
       console.log('bad length', c.length, l)
+      return false
     }
-    for (let i = a0; i < l; i++) {
-      d = c.slice(a160, a192)
-      if (i > a0) {
+    i = a0
+    while (i < l) {
+      e = c.slice(a160, a192)
+      if (i++ > a0) {
         b = false
-        f = d.toString()
-        for (j = a0; j < a5; j++) {
-          k = j * a32
-          if (e.slice(k, k + a32).toString() == f) {
+        f = e.toString()
+        j = a0
+        while (j < a160) {
+          if (g.slice(j, j += a32).toString() == f) {
             b = true
           }
         }
         if (!b) {
-          console.log('bad sign', i, d, a, s)
+          console.log('bad sign', a, e, s)
           return false
         }
       }
-      e = c.slice(a0, a160)
+      g = c.slice(a0, a160)
       c = c.slice(a192)
-      s = c.slice(a0, a16384)
-      c = c.slice(a16384)
-      if (!valid(reduce(e, a32), d, s)) {
-        console.log('bad branch', i, d, reduce(e, a32), s)
+      s = c.slice(a0, a15552)
+      c = c.slice(a15552)
+      if (!valid(reduce(g, a32), e, s)) {
+        console.log('bad branch', e, reduce(g, a32), s)
         return false
       }
     }
     b = false
-    f = g.toString()
-    for (j = a0; j < a5; j++) {
-      k = j * a32
-      if (e.slice(k, k + a32).toString() == f) {
+    f = d.toString()
+    j = a0
+    while (j < a160) {
+      if (g.slice(j, j += a32).toString() == f) {
         b = true
       }
     }
-    if (!b) {
-      console.log('bad last', d, data, si)
-      return false
-    } else {
+    if (b) {
       return true
     }
+    console.log('bad last', f)
   } catch (e) {
     console.log('error', e)
-    return false
   }
+  return false
 }
 
 data = randombytes(a32)
 priv = randombytes(a32)
 pub = gen(expand(priv, a0, a32))
-signature = sig(data, priv)
+signature = sig(priv, data)
 verify(data, pub, signature)
